9153460c38bcfc55dcfaac55ec1adca8cee164fd
zfih <zfihgaming@gmail.com>
2019-11-03 13:07:44 +0100

    Added libtorch to cmake and mai_engine

---
+++ src/mai-engine/mai_engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zfih on 03/11/2019.
+//
+
+#include "mai_engine.hpp"
---
+++ src/mai-engine/mai_engine.hpp
@@ -0,0 +1,16 @@
+//
+// Created by zfih on 03/11/2019.
+//
+
+#ifndef SUPERTUXKART_MAI_ENGINE_HPP
+#define SUPERTUXKART_MAI_ENGINE_HPP
+
+#include <torch>
+
+
+class mai_engine {
+
+};
+
+
+#endif //SUPERTUXKART_MAI_ENGINE_HPP
